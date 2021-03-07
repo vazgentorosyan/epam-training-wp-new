c19f261816de6f407bf0c8b2f8f76b9f98349170
Vazgen Torosyan <vazgentorosyan@gmail.com>
2021-03-07 10:46:37 +0400

    added template for wpconfig

--- config-files/wp-config.php
+++ config-files/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_db' );
+define( 'DB_NAME', '{{ wp_db_name }}' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wp_user' );
+define( 'DB_USER', '{{ wp_mysql_user }}' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'epam_pass' );
+define( 'DB_PASSWORD', '{{ wp_mysql_pass}}' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
